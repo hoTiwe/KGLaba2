d87b4f025074f75de31821a1aa91432f802f2891
Electr1k <gynter37@gmail.com>
2024-10-09 19:34:14 +0300

    main 1

--- KGLaba2/Form1.Designer.cs
+++ KGLaba2/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(1, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(798, 389);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += PictureBox1_Paint;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
--- KGLaba2/Form1.cs
+++ KGLaba2/Form1.cs
@@ -1,54 +1,135 @@
+using System.Drawing;
+using System.IO;
+
 namespace KGLaba2
 {
     public partial class Form1 : Form
     {
+        int pictureWidth = 0;
+        int pictureHeight = 0;
+        int bitCount = 0;
+        int paletteCount = 0;
+
+        byte[] palette;
+        byte[] pixels;
+        Pixel[] outputPixels;
+
+        Graphics graphics;
         public Form1()
         {
             InitializeComponent();
-            try
+            ReadPicture();
+            parsePixels();
+
+            
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Получаем объект Graphics
+            graphics = e.Graphics;
+            int scale = 20;
+            for (int i = 0; i < pictureHeight * pictureWidth; i++)
             {
-                string filePath = @"C:\Users\Magic\source\repos\KGLaba2\KGLaba2\new_picker.glhf";
+                Console.WriteLine($"x: {outputPixels[i].x}; y: {outputPixels[i].x} color: {outputPixels[i].color};");
 
-                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                graphics.FillRectangle(new SolidBrush(outputPixels[i].color), outputPixels[i].x * scale, outputPixels[i].y * scale, scale, scale);
+            }
+        }
+
+        public void ReadPicture()
+        {
+            string filePath = @"../../../new_picker1.glhf";
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                byte[] buffer = new byte[2];    
+                buffer[0] = (byte) fileStream.ReadByte();
+                buffer[1] = (byte) fileStream.ReadByte();
+                pictureWidth = (buffer[0] << 8) | buffer[1];
+                
+                buffer[0] = (byte) fileStream.ReadByte();
+                buffer[1] = (byte) fileStream.ReadByte();
+                pictureHeight = (buffer[0] << 8) | buffer[1];
+
+                bitCount = fileStream.ReadByte();
+
+                buffer[0] = (byte)fileStream.ReadByte();
+                buffer[1] = (byte)fileStream.ReadByte();
+                paletteCount = (buffer[0] << 8) | buffer[1];
+
+                Console.WriteLine($"Width: {pictureWidth}; Width: {pictureHeight};");
+                Console.WriteLine($"bit count: {bitCount}; palette count: {paletteCount};");
+                palette = new byte[4 * paletteCount];
+
+                for (int i = 0; i < 4 * paletteCount; i++)
                 {
-                    Console.WriteLine("Reading binary file in chunks of 2 bytes, then 1 byte:");
+                    palette[i] = (byte)fileStream.ReadByte();
+                }
 
-                    int byteValue;
-                    int counter = 0;
-
-                    while ((byteValue = fileStream.ReadByte()) != -1)
-                    {
-                        // Определяем количество байтов для чтения: 2 байта, затем 1 байт
-                        int bytesToRead = counter % 2 == 0 ? 2 : 1;
-                        byte[] buffer = new byte[bytesToRead];
-                        buffer[0] = (byte)byteValue;
-
-                        // Читаем оставшиеся байты, если это группа из 2 байтов
-                        if (bytesToRead > 1 && fileStream.Read(buffer, 1, bytesToRead - 1) == 0)
-                        {
-                            break; // конец файла
-                        }
-
-                        if (bytesToRead == 2)
-                        {
-                            // Преобразуем два байта в одно число
-                            int combinedValue = (buffer[0] << 8) | buffer[1];
-                            Console.WriteLine($"Bytes read (2): {buffer[0]} {buffer[1]}"); // выводим десятичное число
-                            Console.WriteLine($"Bytes read (2): {combinedValue}"); // выводим десятичное число
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Bytes read (1): {buffer[0]}");
-                        }
-
-                        counter++;
-                    }
+                int countPixelByte = (int) Math.Ceiling((double)pictureWidth * pictureHeight * bitCount / 8);
+                Console.WriteLine($"countPixelByte: {countPixelByte}");
+                pixels = new byte[countPixelByte];
+                for (int i = 0; i < countPixelByte; i++)
+                {
+                    pixels[i] = (byte)fileStream.ReadByte();
+                    Console.WriteLine($"Pixel {i + 1}: {pixels[i]}");
                 }
             }
-            catch (Exception ex)
+        }
+
+        public void parsePixels()
+        {
+            outputPixels = new Pixel[pictureWidth * pictureHeight];
+
+            int countPixelByte = (int)Math.Ceiling((double)pictureWidth * pictureHeight * bitCount / 8);
+            int startPosition = 0;
+            int bitsTotal = bitCount * pictureWidth * pictureHeight;
+
+            while (startPosition + bitCount <= bitsTotal)
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                int value = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    int byteIndex = (startPosition + i) / 8;
+                    int bitIndex = 7 - ((startPosition + i) % 8);
+
+                    // Получаем значение текущего бита
+                    int bit = (pixels[byteIndex] >> bitIndex) & 1;
+
+                    // Сдвигаем бит на нужную позицию и добавляем к значению
+                    value = (value << 1) | bit;
+                }
+
+                outputPixels[startPosition / 5] = getPixelByValue(value, startPosition / 5);
+
+                startPosition += 5;
             }
+        }
+
+        public Pixel getPixelByValue(int value, int index)
+        {
+            int xPalette = value >> bitCount;
+            int yPalette = value & 3;
+
+            int indexPalette = 4 * (xPalette * 5 + yPalette);
+            Console.WriteLine($"index: {index} x: {index % pictureWidth}; y: {index / pictureWidth} [a={palette[indexPalette]}, {palette[indexPalette + 1]}, {palette[indexPalette]+2}, {palette[indexPalette] + 3};");
+
+            return new Pixel(index % pictureWidth, index / pictureWidth, palette[indexPalette], palette[indexPalette + 1], palette[indexPalette + 2], palette[indexPalette + 3]);
+        }
+    }
+
+    public class Pixel
+    {
+        public int x, y;
+        public Color color;
+
+        public Pixel(int x, int y, int alpha, int red, int green, int blue)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = Color.FromArgb(alpha, red, green, blue);
         }
     }
 }
